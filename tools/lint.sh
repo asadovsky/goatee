bc4ad096f41103a56b8d4fa9cc3c3c45ec93761c
Adam Sadovsky <asadovsky@gmail.com>
2013-11-24 17:31:58 -0800

    improve lint file matching

--- tools/lint.sh
+++ tools/lint.sh
@@ -8,7 +8,5 @@ cd $GOPATH
 #find . -name '*.go' -print0 | xargs -0 gofmt -d
 find . -name '*.go' -print0 | xargs -0 gofmt -w
 
-#find . -name '*.js' -print0 | xargs -0 gjslint --nojsdoc --nobeep
-find . -name 'index.js' -print0 | xargs -0 gjslint --nojsdoc --nobeep
-find . -name 'ot_text.js' -print0 | xargs -0 gjslint --nojsdoc --nobeep
-find . -name 'editor.js' -print0 | xargs -0 gjslint --nojsdoc --nobeep
+find . -name '*.js' -not -name 'jquery*.min.js' -not -name 'jasmine*.js' -print0 | \
+  xargs -0 gjslint --nojsdoc --nobeep
