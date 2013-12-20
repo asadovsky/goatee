1d6aa4ecfc29de2687b9a6b389203beb44185661
Adam Sadovsky <asadovsky@gmail.com>
2013-12-20 09:56:31 -0800

    fix some naming issues

--- src/main/index.js
+++ src/main/index.js
@@ -1,20 +1,20 @@
 'use strict';
 
-var ed = document.querySelector('#editor');
+var editorEl = document.querySelector('#editor');
 
 var applyInsert = function(pos, value) {
-  var t = ed.value;
-  ed.value = t.substr(0, pos) + value + t.substr(pos);
+  var t = editorEl.value;
+  editorEl.value = t.substr(0, pos) + value + t.substr(pos);
 };
 
 var applyDelete = function(pos, len) {
-  var t = ed.value;
-  ed.value = t.substr(0, pos) + t.substr(pos + len);
+  var t = editorEl.value;
+  editorEl.value = t.substr(0, pos) + t.substr(pos + len);
 };
 
 var onDocLoaded = function(doc) {
   var model = doc.getModel();
-  ed.value = model.getText();
+  editorEl.value = model.getText();
 
   model.addEventListener(ot.EventType.TEXT_INSERT, applyInsert);
   model.addEventListener(ot.EventType.TEXT_DELETE, applyDelete);
@@ -22,61 +22,61 @@ var onDocLoaded = function(doc) {
   // Use keypress to catch char insertions, keydown to catch backspace/delete.
   // Also catch cut and paste.
   // TODO: Catch undo/redo, maybe using 'input' event.
-  ed.addEventListener('keydown', function(e) {
-    var selStart = ed.selectionStart, selEnd = ed.selectionEnd;
+  editorEl.addEventListener('keydown', function(e) {
+    var selStart = editorEl.selectionStart, selEnd = editorEl.selectionEnd;
     switch (e.which) {
     case 8:  // backspace
       // Handles ctrl+backspace.
       window.setTimeout(function() {
-        var newSelStart = ed.selectionStart;
+        var newSelStart = editorEl.selectionStart;
         var len = selEnd - newSelStart;
         if (len > 0) {
-          model.pushDelete(newSelStart, len);
+          model.deleteText(newSelStart, len);
         }
       }, 0);
       break;
     case 46:  // delete
       // Handles ctrl+delete.
-      var size = ed.value.length;
+      var size = editorEl.value.length;
       window.setTimeout(function() {
-        var newSize = ed.value.length;
+        var newSize = editorEl.value.length;
         var len = size - newSize;
         if (len > 0) {
-          model.pushDelete(selStart, len);
+          model.deleteText(selStart, len);
         }
       }, 0);
       break;
     }
   });
 
-  ed.addEventListener('keypress', function(e) {
-    var selStart = ed.selectionStart, selEnd = ed.selectionEnd;
+  editorEl.addEventListener('keypress', function(e) {
+    var selStart = editorEl.selectionStart, selEnd = editorEl.selectionEnd;
     // If there was a prior selection, log the deletion.
     if (selStart < selEnd) {
-      model.pushDelete(selStart, selEnd - selStart);
+      model.deleteText(selStart, selEnd - selStart);
     }
-    model.pushInsert(selStart, String.fromCharCode(event.which));
+    model.insertText(selStart, String.fromCharCode(event.which));
   });
 
-  ed.addEventListener('cut', function(e) {
-    var selStart = ed.selectionStart, selEnd = ed.selectionEnd;
+  editorEl.addEventListener('cut', function(e) {
+    var selStart = editorEl.selectionStart, selEnd = editorEl.selectionEnd;
     if (selStart < selEnd) {
-      model.pushDelete(selStart, selEnd - selStart);
+      model.deleteText(selStart, selEnd - selStart);
     }
   });
 
-  ed.addEventListener('paste', function(e) {
-    var selStart = ed.selectionStart, selEnd = ed.selectionEnd;
+  editorEl.addEventListener('paste', function(e) {
+    var selStart = editorEl.selectionStart, selEnd = editorEl.selectionEnd;
     // If there was a prior selection, log the deletion.
     if (selStart < selEnd) {
-      model.pushDelete(selStart, selEnd - selStart);
+      model.deleteText(selStart, selEnd - selStart);
     }
     // Get the pasted content.
     window.setTimeout(function() {
-      var newSelStart = ed.selectionStart;
-      model.pushInsert(selStart, ed.value.substr(selStart, newSelStart));
+      var newSelStart = editorEl.selectionStart;
+      model.insertText(selStart, editorEl.value.substr(selStart, newSelStart));
     }, 0);
   });
 };
 
-ot.Load(0, onDocLoaded);
+ot.load(0, onDocLoaded);
--- src/main/ot.js
+++ src/main/ot.js
@@ -14,7 +14,7 @@ ot.DEBUG_DELAY = 0;
 ot.DEBUG_SOCKET = false;
 
 // Similar to gapi.drive.realtime.load.
-ot.Load = function(docId, onDocLoaded) {
+ot.load = function(docId, onDocLoaded) {
   // TODO: What prevents JS from GCing this object?
   new ot.Document(onDocLoaded);
 };
@@ -32,63 +32,63 @@ ot.EventType = {
 
 // Similar to gapi.drive.realtime.Document.
 ot.Document = function(onDocLoaded) {
-  this._onDocLoaded = onDocLoaded;
+  this.onDocLoaded_ = onDocLoaded;
 
   // Initialized by NewClient message from server.
-  this._clientId = null;
-  this._baseTxnId = null;  // last transaction we've gotten from server
+  this.clientId_ = null;
+  this.baseTxnId_ = null;  // last transaction we've gotten from server
 
   // The most recent clientOps index sent to and acknowledged by the server.
-  this._sentClientOpIdx = -1;
-  this._ackedClientOpIdx = -1;
+  this.sentClientOpIdx_ = -1;
+  this.ackedClientOpIdx_ = -1;
 
   // All past client ops. Bridge from latest server-acked state to client state
   // starts at clientOps[ackedClientOpIdx] + 1.
-  this._clientOps = [];
+  this.clientOps_ = [];
 
-  this._socket = new WebSocket(document.body.getAttribute('data-ws-url'));
-  this._model = null;  // initialized in socket.onmessage
+  this.socket_ = new WebSocket(document.body.getAttribute('data-ws-url'));
+  this.model_ = null;  // initialized in socket.onmessage
 
-  this._socket.onclose = (function(event) {
+  this.socket_.onclose = (function(event) {
     if (ot.DEBUG_SOCKET) console.log('socket.close');
   }).bind(this);
 
-  this._socket.onmessage = (function(event) {
+  this.socket_.onmessage = (function(event) {
     if (ot.DEBUG_SOCKET) console.log('socket.receive ' + event.data);
     var msg = JSON.parse(event.data);
     // TODO: Implement better way to detect message type.
     if (msg.hasOwnProperty('Text')) {  // msg type NewClient
-      console.assert(this._clientId === null);
-      this._clientId = msg['ClientId'];
-      this._baseTxnId = parseInt(msg['BaseTxnId']);
-      this._model = new ot.Model(this, msg['Text']);
-      this._onDocLoaded(this);
+      console.assert(this.clientId_ === null);
+      this.clientId_ = msg['ClientId'];
+      this.baseTxnId_ = parseInt(msg['BaseTxnId']);
+      this.model_ = new ot.Model(this, msg['Text']);
+      this.onDocLoaded_(this);
       return;
     }
 
     console.assert(msg.hasOwnProperty('TxnId'));  // msg type Broadcast
     var newBaseTxnId = parseInt(msg['TxnId']);
-    console.assert(newBaseTxnId === this._baseTxnId + 1);
-    this._baseTxnId = newBaseTxnId;
+    console.assert(newBaseTxnId === this.baseTxnId_ + 1);
+    this.baseTxnId_ = newBaseTxnId;
 
     // If txn is from this client, send all buffered ops to server.
     // Otherwise, transform it against all buffered ops and then apply it.
-    if (msg['ClientId'] === this._clientId) {
-      this._ackedClientOpIdx = this._sentClientOpIdx;
+    if (msg['ClientId'] === this.clientId_) {
+      this.ackedClientOpIdx_ = this.sentClientOpIdx_;
       this._sendBufferedOps();
       return;
     }
     var ops = opsFromStrings(msg['OpStrs']);
     var tup = transformCompound(
-      this._clientOps.slice(this._ackedClientOpIdx + 1), ops);
+      this.clientOps_.slice(this.ackedClientOpIdx_ + 1), ops);
     var bufferedOps = tup[0];
     ops = tup[1];
     // Unfortunately, splice doesn't support Array inputs.
     for (var i = 0; i < bufferedOps.length; i++) {
-      this._clientOps[this._ackedClientOpIdx + 1 + i] = bufferedOps[i];
+      this.clientOps_[this.ackedClientOpIdx_ + 1 + i] = bufferedOps[i];
     }
     // Apply the transformed server compound op against the client text.
-    this._model._applyCompound(ops);
+    this.model_.applyCompound_(ops);
   }).bind(this);
 };
 
@@ -97,25 +97,25 @@ ot.Document.prototype.getCollaborators = function() {
 };
 
 ot.Document.prototype.getModel = function() {
-  return this._model;
+  return this.model_;
 };
 
 ot.Document.prototype._sendBufferedOps = function() {
-  console.assert(this._sentClientOpIdx === this._ackedClientOpIdx);
-  if (this._sentClientOpIdx === this._clientOps.length - 1) {
+  console.assert(this.sentClientOpIdx_ === this.ackedClientOpIdx_);
+  if (this.sentClientOpIdx_ === this.clientOps_.length - 1) {
     return;  // no ops to send
   }
-  this._sentClientOpIdx = this._clientOps.length - 1;
+  this.sentClientOpIdx_ = this.clientOps_.length - 1;
   // TODO: Compress ops (e.g. combine insertions) before sending.
   var msg = {
-    'OpStrs': opsToStrings(this._clientOps.slice(this._ackedClientOpIdx + 1)),
-    'ClientId': this._clientId,
-    'BaseTxnId': this._baseTxnId
+    'OpStrs': opsToStrings(this.clientOps_.slice(this.ackedClientOpIdx_ + 1)),
+    'ClientId': this.clientId_,
+    'BaseTxnId': this.baseTxnId_
   };
   var send = (function() {
     var json = JSON.stringify(msg);
     if (ot.DEBUG_SOCKET) console.log('socket.send ' + json);
-    this._socket.send(json);
+    this.socket_.send(json);
   }).bind(this);
   if (ot.DEBUG_DELAY > 0) {
     window.setTimeout(send, ot.DEBUG_DELAY);
@@ -125,11 +125,11 @@ ot.Document.prototype._sendBufferedOps = function() {
 };
 
 ot.Document.prototype._pushOp = function(op) {
-  var clientOpIdx = this._clientOps.length;
-  this._clientOps.push(op);
+  var clientOpIdx = this.clientOps_.length;
+  this.clientOps_.push(op);
   // If op is parented off server state space (as opposed to some non-acked
   // client op), send it right away.
-  if (clientOpIdx === this._ackedClientOpIdx + 1) {
+  if (clientOpIdx === this.ackedClientOpIdx_ + 1) {
     this._sendBufferedOps();
   }
 };
@@ -140,27 +140,27 @@ ot.Document.prototype._pushOp = function(op) {
 // Currently, a combination of gapi.drive.realtime.Model and
 // gapi.drive.realtime.CollaborativeString.
 ot.Model = function(doc, text) {
-  this._doc = doc;
-  this._text = text;
-  this._listeners = {};
-  this._listeners[ot.EventType.TEXT_INSERT] = [];
-  this._listeners[ot.EventType.TEXT_DELETE] = [];
+  this.doc_ = doc;
+  this.text_ = text;
+  this.listeners_ = {};
+  this.listeners_[ot.EventType.TEXT_INSERT] = [];
+  this.listeners_[ot.EventType.TEXT_DELETE] = [];
 };
 
-ot.Model.prototype.pushInsert = function(pos, value) {
-  this._doc._pushOp(new Insert(pos, value));
+ot.Model.prototype.insertText = function(pos, value) {
+  this.doc_._pushOp(new Insert(pos, value));
 };
 
-ot.Model.prototype.pushDelete = function(pos, len) {
-  this._doc._pushOp(new Delete(pos, len));
+ot.Model.prototype.deleteText = function(pos, len) {
+  this.doc_._pushOp(new Delete(pos, len));
 };
 
 ot.Model.prototype.addEventListener = function(type, handler) {
-  this._listeners[type].push(handler);
+  this.listeners_[type].push(handler);
 };
 
 ot.Model.prototype.removeEventListener = function(type, handler) {
-  var arr = this._listeners[type];
+  var arr = this.listeners_[type];
   while (true) {
     var i = arr.indexOf(handler);
     if (i === -1) break;
@@ -177,23 +177,23 @@ ot.Model.prototype.redo = function() {
 };
 
 ot.Model.prototype.getText = function() {
-  return this._text;
+  return this.text_;
 };
 
-ot.Model.prototype._apply = function(op) {
-  var t = this._text;
+ot.Model.prototype.apply_ = function(op) {
+  var t = this.text_;
   switch (op.typeName()) {
   case 'Insert':
-    this._text = t.substr(0, op.pos) + op.value + t.substr(op.pos);
-    var arr = this._listeners[ot.EventType.TEXT_INSERT];
+    this.text_ = t.substr(0, op.pos) + op.value + t.substr(op.pos);
+    var arr = this.listeners_[ot.EventType.TEXT_INSERT];
     for (var i = 0; i < arr.length; i++) {
       arr[i](op.pos, op.value);
     }
     break;
   case 'Delete':
     console.assert(op.pos + op.len <= t.length, 'Delete past end');
-    this._text = t.substr(0, op.pos) + t.substr(op.pos + op.len);
-    var arr = this._listeners[ot.EventType.TEXT_DELETE];
+    this.text_ = t.substr(0, op.pos) + t.substr(op.pos + op.len);
+    var arr = this.listeners_[ot.EventType.TEXT_DELETE];
     for (var i = 0; i < arr.length; i++) {
       arr[i](op.pos, op.len);
     }
@@ -203,8 +203,8 @@ ot.Model.prototype._apply = function(op) {
   }
 };
 
-ot.Model.prototype._applyCompound = function(ops) {
+ot.Model.prototype.applyCompound_ = function(ops) {
   for (var i = 0; i < ops.length; i++) {
-    this._apply(ops[i]);
+    this.apply_(ops[i]);
   }
 };
